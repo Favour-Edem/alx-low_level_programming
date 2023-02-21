444cade8b6f7468b061c520d48f0556acc811a41
Favour-Edem <fave0524@gmail.com>
2023-02-21 15:09:12 -0800

    Print lowercase alphabets program

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,13 @@
 *
 * Return: Always 0 (Success)
 */
-void print_alphabet(void);
+
+void print_alphabet(void)
 {
 int j;
 for (j = 'a'; j <= 'z'; j++)
 {
 _putchar(j);
 }
-_putchar('\n')
+_putchar('\n');
 }
