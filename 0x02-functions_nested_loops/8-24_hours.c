e7b2cfbc42220458b020975a8b712ae00093a9dd
Favour-Edem <fave0524@gmail.com>
2023-02-23 07:08:30 -0800

    Printed every minute of 24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,35 +7,28 @@
  */
 void jack_bauer(void)
 {
-int i, j;
+int i, j, k, l;
 
-for (i = 0; j < 24; i++)
+for (i = 0; i <= 2; i++)
 {
-for (j = 0; j < 60; j++)
+for (j = 0; j <= 9; j++)
 {
-if (i < 10)
+if ((i <= 1 && j <= 9) || (i <= 2 && j <= 3))
 {
-_putchar('0');
+for (k = 0; k <= 5; k++)
+{
+for (l = 0; l <= 9; l++)
+{
 _putchar(i + '0');
-}
-else if (i >= 10)
-{
-_putchar((i / 10) + '0');
-_putchar((i % 10) + '0');
-}
-if (j < 10)
-{
-_putchar(':');
-_putchar('0');
-_putchar(j);
-}
-else if (j >= 10)
-{
-_putchar(':');
-_putchar((j / 10) + '0');
-_putchar((j % 10) + '0');
-}
+_putchar(j + '0');
+_putchar(58);
+_putchar(k + '0');
+_putchar(l + '0');
 _putchar('\n');
 }
 }
 }
+}
+}
+}
+
