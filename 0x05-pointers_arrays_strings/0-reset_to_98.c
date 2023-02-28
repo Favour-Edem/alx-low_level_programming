f1fd57284a33116f0f0e3fb1867ce28bf501ed5b
Favour-Edem <fave0524@gmail.com>
2023-02-28 05:34:21 -0800

    Updated the value of an integer ponter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates the value of an integer
+ * pointed at to 98
+ * @n: an integer pointer
+ * Return: empty
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+A directory that contains C programs on projects for C-pointers, C-arays and C-strings
