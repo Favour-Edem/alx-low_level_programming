f3125fb8bfbbdb4d7ccadbdcbf30ac550b7fa837
Favour-Edem <fave0524@gmail.com>
2023-03-21 05:46:46 -0700

    Defined the macro SIZE

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,17 @@
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @SIZE: defined macro in header file
+ * Return: Always 0.
+ */
+int main(void)
+{
+int s;
+
+s = 98 + SIZE;
+printf("%d\n", s);
+return (0);
+}
