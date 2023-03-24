fa24aac9d6904f201898b701f7f7edf4ab3f86bb
Favour-Edem <fave0524@gmail.com>
2023-03-24 06:47:00 -0700

    Printed multiple arguments

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,19 +13,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 unsigned int i;
 va_list ap;
 
-if (n == 0)
+if (separator == NULL)
 {
-return;
+separator = "";
 }
 
 va_start(ap, n);
 
 for (i = 0; i < n; i++)
 {
-if (separator == NULL)
-{
-printf("%d", va_arg(ap, int));
-}
 if (i != (n - 1))
 {
 printf("%d%s", va_arg(ap, int), separator);
