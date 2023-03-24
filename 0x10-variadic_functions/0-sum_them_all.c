bb29e1bd964579e99202e9f2e9c39a5d1b80e224
Favour-Edem <fave0524@gmail.com>
2023-03-23 19:08:01 -0700

    Summed all arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,12 +14,16 @@ unsigned int i, sum = 0;
 va_list list;
 
 if (n == 0)
+{
 return (0);
+}
 
 va_start(list, n);
 
 for (i = 0; i < n; i++)
+{
 sum += va_arg(list, int);
+}
 
 va_end(list);
 return (sum);
