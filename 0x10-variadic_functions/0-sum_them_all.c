47c5b0f7de639d42fbf90eebe7cf1459aba5b7af
Favour-Edem <fave0524@gmail.com>
2023-03-24 06:54:47 -0700

    Summed up multiple arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,18 +5,15 @@
 /**
  * sum_them_all - function to be used
  * @n: number of arguments to be passed
- * Return: Always 0,Success
+ * Return: Always 0, Success
  */
 int sum_them_all(const unsigned int n, ...)
 {
 unsigned int i, sum = 0;
-
 va_list list;
 
 if (n == 0)
-{
 return (0);
-}
 
 va_start(list, n);
 
